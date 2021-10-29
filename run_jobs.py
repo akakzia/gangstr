7eb9c779bc3a6abbe743fdbe3a11c05e9042d7dc
ahmed <ahmed.akakzia@gmail.com>
2021-10-29 17:45:34 +0200

    update job runner

--- run_jobs.py
+++ run_jobs.py
@@ -14,33 +14,31 @@ scratch = os.environ['SCRATCH']
 mkdir_p(job_directory)
 
 nb_seeds = 10
-values = [1.0, 0.2, 0.05, 0.005, 0.002, 0.001, 0.0]
 
 for i in range(nb_seeds):
-    for value in values:
-        job_file = os.path.join(job_directory, "main_{}%.slurm".format(value*100))
+    job_file = os.path.join(job_directory, "value_disagreement_%.slurm")
 
-        with open(job_file, 'w') as fh:
-            fh.writelines("#!/bin/bash\n")
-            fh.writelines("#SBATCH --account=oke@cpu\n")
-            fh.writelines("#SBATCH --job-name=main_{}\n".format(value))
-            fh.writelines("#SBATCH --partition=cpu_p1\n")
-            fh.writelines("#SBATCH --qos=qos_cpu-t4\n")
-            fh.writelines("#SBATCH --output=main_{}%_%j.out\n".format(100*value))
-            fh.writelines("#SBATCH --error=main_{}%_%j.out\n".format(100*value))
-            fh.writelines("#SBATCH --time=40:00:00\n")
-            fh.writelines("#SBATCH --nodes=1\n")
-            fh.writelines("#SBATCH --ntasks=24\n")
-            fh.writelines("#SBATCH --hint=nomultithread\n")
+    with open(job_file, 'w') as fh:
+        fh.writelines("#!/bin/bash\n")
+        fh.writelines("#SBATCH --account=oke@cpu\n")
+        fh.writelines("#SBATCH --job-name=vd_baseline_{}\n".format(i))
+        fh.writelines("#SBATCH --partition=cpu_p1\n")
+        fh.writelines("#SBATCH --qos=qos_cpu-t4\n")
+        fh.writelines("#SBATCH --output=vd_baseline_{}%_%j.out\n".format(i))
+        fh.writelines("#SBATCH --error=vd_baseline_{}%_%j.out\n".format(i))
+        fh.writelines("#SBATCH --time=40:00:00\n")
+        fh.writelines("#SBATCH --nodes=1\n")
+        fh.writelines("#SBATCH --ntasks=24\n")
+        fh.writelines("#SBATCH --hint=nomultithread\n")
 
-            fh.writelines("module load pytorch-cpu/py3/1.4.0\n")
+        fh.writelines("module load pytorch-cpu/py3/1.4.0\n")
 
-            fh.writelines("export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/gpfslocalsup/spack_soft/mesa/18.3.6/gcc-9.1.0-bikg6w3g2be2otzrmyy43zddre4jahme/lib\n")
-            fh.writelines("export LIBRARY_PATH=$LIBRARY_PATH:/gpfslocalsup/spack_soft/mesa/18.3.6/gcc-9.1.0-bikg6w3g2be2otzrmyy43zddre4jahme/lib\n")
-            fh.writelines("export CPATH=$CPATH:/gpfslocalsup/spack_soft/mesa/18.3.6/gcc-9.1.0-bikg6w3g2be2otzrmyy43zddre4jahme/include\n")
-            fh.writelines("export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/linkhome/rech/genisi01/uqy56ga/.mujoco/mujoco200/bin\n")
+        fh.writelines("export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/gpfslocalsup/spack_soft/mesa/18.3.6/gcc-9.1.0-bikg6w3g2be2otzrmyy43zddre4jahme/lib\n")
+        fh.writelines("export LIBRARY_PATH=$LIBRARY_PATH:/gpfslocalsup/spack_soft/mesa/18.3.6/gcc-9.1.0-bikg6w3g2be2otzrmyy43zddre4jahme/lib\n")
+        fh.writelines("export CPATH=$CPATH:/gpfslocalsup/spack_soft/mesa/18.3.6/gcc-9.1.0-bikg6w3g2be2otzrmyy43zddre4jahme/include\n")
+        fh.writelines("export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/linkhome/rech/genisi01/uqy56ga/.mujoco/mujoco200/bin\n")
 
-            fh.writelines("srun python -u -B train.py --intervention-prob {} --save-dir 'main_{}%/' 2>&1 ".format(value, value*100))
+        fh.writelines("srun python -u -B train.py --algo 'value_disagreement' --save-dir 'vd_baseline/' 2>&1 ")
 
-        os.system("sbatch %s" % job_file)
-        sleep(1)
+    os.system("sbatch %s" % job_file)
+    sleep(1)
